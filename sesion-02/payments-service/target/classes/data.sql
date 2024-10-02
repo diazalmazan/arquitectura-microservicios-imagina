571780fc323dcffe9a2764fd21231e1927d3e5e1
dmartinezsepulveda.external@epo.org <dmartinezsepulveda.external@epo.org>
2024-10-02 10:28:55 +0200

    adding sesion-02

---
+++ sesion-02/payments-service/target/classes/data.sql
@@ -0,0 +1,3 @@
+-- Datos iniciales para la tabla PAGO
+INSERT INTO PAYMENT (order_id, amount, discount) VALUES (1, 4500, 'CHRISTMAS');
+INSERT INTO PAYMENT (order_id, amount, discount) VALUES (2, 1600, NULL);
